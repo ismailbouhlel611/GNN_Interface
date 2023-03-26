367db7e4466fe66e6230587618743765bbc80764
ismailbouhlel611 <ismail.bouhlel@ensi-uma.tn>
2023-03-26 15:55:56 +0100

    first graph

--- interface/src/views/admin/default/components/Graph.js
+++ interface/src/views/admin/default/components/Graph.js
@@ -1,35 +1,52 @@
 import vis from 'vis';
-import React from 'react';
-import './Graph.css'
+import React, { useState, useEffect } from 'react';
+import { DataSet } from 'vis-data';
+import { Network } from 'vis-network';
+import './Graph.css';
+import nodesData from './nodes22.json';
+import edgesData from './edges.json';
+
+console.log(nodesData.label);
 
 export default class MyVisComponent extends React.Component {
-    // ... component code here ...
-    componentDidMount() {
-        // create a new network instance
-        const container = document.getElementById('mynetwork');
-        const data = {
-          nodes: [
-            { id: 1, label: 'Node 1' },
-            { id: 2, label: 'Node 2' },
-            { id: 3, label: 'Node 3' },
-            { id: 4, label: 'Node 4' },
-            { id: 5, label: 'Node 5' }
-          ],
-          edges: [
-            { from: 1, to: 2 },
-            { from: 1, to: 3 },
-            { from: 2, to: 4 },
-            { from: 2, to: 5 }
-          ]
-        };
-        const options = {};
-        const network = new vis.Network(container, data, options);
-      };
-    render() {
-        return (
-          <div>
-            <div id="mynetwork" ></div>
-          </div>
-        );
-      };
+  // ... component code here ...
+  componentDidMount() {
+    // create a new network instance
+    var nodes=nodesData;
+    var edges=edgesData;
+    const container = document.getElementById('mynetwork');
+    const data = {
+      nodes:nodes,
+      edges: edges
+    };
+
+    
+    const options = {
+      "edges": {
+        "smooth": {
+          "forceDirection": "none"
+        }
+      },
+      "physics": {
+        "barnesHut": {
+          "gravitationalConstant": -17300,
+          "centralGravity": 0,
+          "springConstant": 0.05,
+          "avoidOverlap": 0.12
+        },
+        "maxVelocity": 0.1,
+        "minVelocity": 0
+      }
+    };
+    
+    const network = new vis.Network(container, data, options);
+  };
+  render() {
+    return (
+      <div>
+        <div id="mynetwork" ></div>
+      </div>
+    );
   }
+  
+}
--- interface/src/views/admin/default/index.jsx
+++ interface/src/views/admin/default/index.jsx
@@ -22,55 +22,53 @@
 
 // Chakra imports
 import {
-  Avatar,
+  // Avatar,
   Box,
-  Flex,
-  FormLabel,
-  Icon,
-  Select,
-  SimpleGrid,
+  // Flex,
+  // FormLabel,
+  // Icon,
+  // Select,
+  // SimpleGrid,
   useColorModeValue,
 } from "@chakra-ui/react";
 // Assets
-import Usa from "assets/img/dashboards/usa.png";
-// Custom components
-import MiniCalendar from "components/calendar/MiniCalendar";
-import MiniStatistics from "components/card/MiniStatistics";
-import IconBox from "components/icons/IconBox";
-import React from "react";
-import {
-  MdAddTask,
-  MdAttachMoney,
-  MdBarChart,
-  MdFileCopy,
-} from "react-icons/md";
-import CheckTable from "views/admin/default/components/CheckTable";
-import ComplexTable from "views/admin/default/components/ComplexTable";
-import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
-import Tasks from "views/admin/default/components/Tasks";
-import TotalSpent from "views/admin/default/components/TotalSpent";
-import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
-import {
-  columnsDataCheck,
-  columnsDataComplex,
-} from "views/admin/default/variables/columnsData";
-import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
-import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
-import MyVisComponent from './components/Graph';
-import './index.css'
+// import Usa from "assets/img/dashboards/usa.png";
+// // Custom components
+// import MiniCalendar from "components/calendar/MiniCalendar";
+// import MiniStatistics from "components/card/MiniStatistics";
+// import IconBox from "components/icons/IconBox";
+// import React from "react";
+// import {
+//   MdAddTask,
+//   MdAttachMoney,
+//   MdBarChart,
+//   MdFileCopy,
+// } from "react-icons/md";
+// import CheckTable from "views/admin/default/components/CheckTable";
+// import ComplexTable from "views/admin/default/components/ComplexTable";
+// import DailyTraffic from "views/admin/default/components/DailyTraffic";
+// import PieCard from "views/admin/default/components/PieCard";
+// import Tasks from "views/admin/default/components/Tasks";
+// import TotalSpent from "views/admin/default/components/TotalSpent";
+// import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
+// import {
+//   columnsDataCheck,
+//   columnsDataComplex,
+// } from "views/admin/default/variables/columnsData";
+// import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
+// import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
+import MyVisComponent  from './components/Graph';
+import './index.css';
 
 export default function UserReports() {
   // Chakra Color Mode
   const brandColor = useColorModeValue("brand.500", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
-  
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <div className="graph_container">
-      <MyVisComponent />
+      <MyVisComponent  />
      </div>
-
       {/* <SimpleGrid
         columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
         gap='20px'

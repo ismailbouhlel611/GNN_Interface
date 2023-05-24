1ed3e84b24623de530eed2684915a04ab32ffa4f
ismailbouhlel611 <ismail.bouhlel@ensi-uma.tn>
2023-05-24 03:13:06 +0100

    done changes

--- auth_system/auth_system/settings.py
+++ auth_system/auth_system/settings.py
@@ -115,7 +115,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'ad.gnn.interface@gmail.com'
-EMAIL_HOST_PASSWORD = 'lbiztezqhnxpqsjn'
+EMAIL_HOST_PASSWORD = 'zkibuavkwndftgho'
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 
